4f2c08f7d54675cc63be01ce04d8c3860d50e04c
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-02-08 11:33:55 +0100

    Add files via upload

---
+++ KolekcijeVjezba/KolekcijeVjezba/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace KolekcijeVjezba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.btnIspisi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.cmbRod = new System.Windows.Forms.ComboBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtGodina = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(154, 12);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(141, 154);
+            this.btnUnesi.TabIndex = 0;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.Location = new System.Drawing.Point(301, 12);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(141, 154);
+            this.btnObradi.TabIndex = 1;
+            this.btnObradi.Text = "Obradi";
+            this.btnObradi.UseVisualStyleBackColor = true;
+            this.btnObradi.Click += new System.EventHandler(this.btnObradi_Click);
+            // 
+            // btnIspisi
+            // 
+            this.btnIspisi.Location = new System.Drawing.Point(448, 12);
+            this.btnIspisi.Name = "btnIspisi";
+            this.btnIspisi.Size = new System.Drawing.Size(141, 154);
+            this.btnIspisi.TabIndex = 2;
+            this.btnIspisi.Text = "Ispiši";
+            this.btnIspisi.UseVisualStyleBackColor = true;
+            this.btnIspisi.Click += new System.EventHandler(this.btnIspisi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Podatak 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Podatak 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Podatak 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Podatak 4";
+            // 
+            // rtbIspis
+            // 
+            this.rtbIspis.Location = new System.Drawing.Point(12, 172);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(577, 266);
+            this.rtbIspis.TabIndex = 8;
+            this.rtbIspis.Text = "";
+            // 
+            // cmbRod
+            // 
+            this.cmbRod.FormattingEnabled = true;
+            this.cmbRod.Items.AddRange(new object[] {
+            "M",
+            "Ž"});
+            this.cmbRod.Location = new System.Drawing.Point(12, 145);
+            this.cmbRod.Name = "cmbRod";
+            this.cmbRod.Size = new System.Drawing.Size(121, 21);
+            this.cmbRod.TabIndex = 9;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(12, 28);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(121, 20);
+            this.txtIme.TabIndex = 10;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(12, 67);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(121, 20);
+            this.txtPrezime.TabIndex = 11;
+            // 
+            // txtGodina
+            // 
+            this.txtGodina.Location = new System.Drawing.Point(12, 106);
+            this.txtGodina.Name = "txtGodina";
+            this.txtGodina.Size = new System.Drawing.Size(121, 20);
+            this.txtGodina.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.txtGodina);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.cmbRod);
+            this.Controls.Add(this.rtbIspis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIspisi);
+            this.Controls.Add(this.btnObradi);
+            this.Controls.Add(this.btnUnesi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Button btnIspisi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtbIspis;
+        private System.Windows.Forms.ComboBox cmbRod;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtGodina;
+    }
+}
+
---
+++ KolekcijeVjezba/KolekcijeVjezba/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolekcijeVjezba
+{
+    public partial class Form1 : Form
+    {
+        List<Osoba> osobe = new List<Osoba>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Osoba osoba = new Osoba(txtIme.Text, txtPrezime.Text, Convert.ToInt32(txtGodina.Text), cmbRod.Text, "");
+
+                osobe.Add(osoba);
+
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtGodina.Clear();
+
+                MessageBox.Show("Uspješan unos!", "Obavijest", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show("Pogrešan unos!\r\n" + greska.Message, "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnIspisi_Click(object sender, EventArgs e)
+        {
+            rtbIspis.AppendText("Ime" + "\tPrezime" + "\tGodina" + "\tSpol" + "\tDodatak" + "\r\n");
+            foreach (Osoba osoba in osobe)
+            {
+                rtbIspis.AppendText(osoba.ToString() + "\r\n");
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            foreach (Osoba osoba in osobe)
+            {
+                if (osoba.Spol == "M")
+                {
+                    osoba.Dodatak = "Ima brkove";
+                }
+                else
+                {
+                    osoba.Dodatak = "Nema brkove";
+                }
+
+            }
+            foreach (Osoba osoba in osobe)
+            {
+                rtbIspis.AppendText(osoba.ToString() + "\r\n");
+            }
+        }
+    }
+}
